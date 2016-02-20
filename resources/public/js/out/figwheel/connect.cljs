7fd1f2de9f83c6d77d0778adf33b2d43d51be131
Zekna <zsharpsquared@gmail.com>
2016-02-19 16:18:14 -0800

    .gitignore compiled js files

--- resources/public/js/out/figwheel/connect.cljs
+++
@@ -1,3 +0,0 @@
-(ns figwheel.connect (:require [figwheel.client] [figwheel.client.utils] [nihl.core]))
-(figwheel.client/start {:build-id "nihl", :websocket-url "ws://localhost:3449/figwheel-ws"})
-
